cbfb8456ee6d7fd81358302c37d78da813fef0bd
2Turu <huynh.t.timothy@gmail.com>
2019-06-10 22:24:29 -0700

    Setup

--- src/data/events/index.js
+++ src/data/events/index.js
@@ -20,8 +20,46 @@ const register = async ( { sql, getConnection } ) => {
        return request.query( sqlQueries.getEvents );
    };
 
+   const addEvent = async ( { userId, title, description, startDate, startTime, endDate, endTime } ) => {
+       const pool = await getConnection();
+       const request = await pool.request();
+       request.input( "userId", sql.VarChar( 50 ), userId );
+       request.input( "title", sql.NVarChar( 200 ), title );
+       request.input( "description", sql.NVarChar( 1000 ), description );
+       request.input( "startDate", sql.Date, startDate );
+       request.input( "startTime", sql.Time, startTime );
+       request.input( "endDate", sql.Date, endDate );
+       request.input( "endTime", sql.Time, endTime );
+       return request.query( sqlQueries.addEvent );
+   };
+
+   const updateEvent = async ( { id, userId, title, description, startDate, startTime, endDate, endTime } ) => {
+       const pool = await getConnection();
+       const request = await pool.request();
+       request.input( "id", sql.Int, id );
+       request.input( "userId", sql.VarChar( 50 ), userId );
+       request.input( "title", sql.NVarChar( 200 ), title );
+       request.input( "description", sql.NVarChar( 1000 ), description );
+       request.input( "startDate", sql.Date, startDate );
+       request.input( "startTime", sql.Time, startTime );
+       request.input( "endDate", sql.Date, endDate );
+       request.input( "endTime", sql.Time, endTime );
+       return request.query( sqlQueries.updateEvent );
+   };
+
+   const deleteEvent = async ( { id, userId } ) => {
+       const pool = await getConnection();
+       const request = await pool.request();
+       request.input( "id", sql.Int, id );
+       request.input( "userId", sql.VarChar( 50 ), userId );
+       return request.query( sqlQueries.deleteEvent );
+   };
+
    return {
-       getEvents
+       addEvent,
+       deleteEvent,
+       getEvents,
+       updateEvent
    };
 };
 
